5b09754584623921902b8e041ea5962375f43032
gconnell29 <gconnelljr29@gmail.com>
2020-02-12 22:35:59 -0500

    completed first 5 challenges

--- index.js
+++ index.js
@@ -39,8 +39,13 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(personId, personName, personEmail) {
+  const person = {
+    id: personId,
+    name: personName,
+    email: personEmail
+  }
+  return person;
 }
 
 /**
@@ -56,8 +61,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return `Hello, my name is ${person.name}`;
 }
 
 /**
@@ -73,8 +78,17 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(personName) {
+  const person = {
+    name: personName,
+    sum: function(num1, num2) {
+      return num1 + num2;
+    },
+    speak: function(personName) {
+      return `Hello, my name is ${name}`
+    }
+  }
+  return person;
 }
 
 
